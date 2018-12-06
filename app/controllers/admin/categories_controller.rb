5291efce3eb02bed5fab1b0d6904ba7f420caa7c
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-06 22:32:59 +0000

    added categories to the dropdown, added method to new.html.erb for creating categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -16,4 +16,13 @@ class Admin::CategoriesController < ApplicationController
         render :new
       end
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
 end
