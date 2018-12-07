bcb60b910f9695ed69bf4a02029f850abb5ee1f4
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-07 20:08:11 +0000

    fixed session issues. still need to fix cart issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+  helper_method :current_user
+
   private
 
   def cart
@@ -28,5 +37,6 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
 
 end
