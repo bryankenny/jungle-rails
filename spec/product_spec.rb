887ca7c7122477f07e35aef762c3b1e5e332eae7
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-11 18:11:00 +0000

    rspec functionality and bundle install

---
+++ spec/product_spec.rb
@@ -0,0 +1,9 @@
+# RSpec.describe Product, type: :model do
+#   describe 'Validations' do
+#     # validation tests/examples here
+#     validates :name, presence: true
+#     validates :price, presence: true
+#     validates :quantity, presence: true
+#     validates :category, presence: true
+#   end
+# end
