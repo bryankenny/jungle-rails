54053825b93d6eed21361fe7f60c888eebd0b253
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-11 22:02:22 +0000

    product_detals_spec rendering individual products

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see an individual product" do
+    visit root_path
+    save_screenshot
+    click_link("Details »", match: :first)
+    expect(page).to have_css 'section.products-show', count: 1
+    save_and_open_screenshot
+
+  end
+end
