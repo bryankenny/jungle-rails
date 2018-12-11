6927123e6bd54908210fa8a177ae3ff4b6b436e0
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-11 20:49:50 +0000

    tests for user model working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
 
-  validates :email, presence: true, uniqueness: true
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = self.where("lower(email) = ?", email.delete(' ').downcase).first.try(:authenticate, password)
+  end
 end
