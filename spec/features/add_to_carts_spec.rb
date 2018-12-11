0076b654639f25b976bcdb742d998c0450182b77
Bryan Kenny <b.93.kenny@gmail.com>
2018-12-11 23:13:12 +0000

    add_to_carts_spec working

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
-
-RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
-
-  # SETUP
+ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
-
-    10.times do |n|
+     10.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -16,12 +13,10 @@ RSpec.feature "Visitor navigates to product details page", type: :feature, js: t
       )
     end
   end
-
-  scenario "They see an individual product" do
+   scenario "They add a product to the cart" do
     visit root_path
-    click_link("Details »", match: :first)
-    expect(page).to have_css 'section.products-show', count: 1
+    click_button("Add", match: :first)
+    expect(page).to have_content("My Cart (1)")
     save_and_open_screenshot
   end
-
 end
